0ce01cac968f5b8bfbb38716a6690ad192dd0c9d
Serapckrkc <Serapckrkc@gmail.com>
2022-01-17 22:47:05 +0300

    project5 functionedited and project6 created

--- FlexPanelGallery/app.js
+++ FlexPanelGallery/app.js
@@ -1,16 +1,24 @@
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-    console.log('hello');
+  
+    panels.forEach(panel => {
+        const isOpen = panel.classList.contains('open')
+        if (isOpen === true && panel !== this ){
+            panel.classList.remove('open')
+        }
+        
+    })
     this.classList.toggle('open');
 }
 
 function toggleActive(e) {
-    console.log(e.propertyName);
+  
     if(e.propertyName.includes('flex')){
         this.classList.toggle('open-active');
     }
     
 }
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
