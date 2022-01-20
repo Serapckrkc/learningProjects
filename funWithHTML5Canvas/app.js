8d8a0ca866ee229ee483fde17d4eecb98abd1ad6
Serapckrkc <Serapckrkc@gmail.com>
2022-01-20 22:24:35 +0300

    project8 finished

--- funWithHTML5Canvas/app.js
+++ funWithHTML5Canvas/app.js
@@ -6,19 +6,41 @@ ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
+
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = false;
 
 function draw(e) {
     if(!isDrawing) return;
     console.log(e)
+    ctx.strokeStyle =`hsl(${hue}, 100%, 50%)` 
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
    
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+
+    hue++;
+    if(hue >= 360){
+        hue = 0;
+    }
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+        
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    }
+    else {
+        ctx.lineWidth--;
+    }
    
 }
 canvas.addEventListener('mousedown', (e) => {
