48134606a7429f8799cc37911a60007516260521
Serapckrkc <Serapckrkc@gmail.com>
2022-01-24 21:57:04 +0300

    project11 finished

---
+++ KeySequenceDetection/app.js
@@ -0,0 +1,15 @@
+const pressed = [];
+const secretCode = 'wesbos';
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    console.log(pressed);
+
+    if(pressed.join('').includes(secretCode)) {
+        console.log('DING DING!');
+        cornify_add();
+    }
+    console.log(pressed);
+});
