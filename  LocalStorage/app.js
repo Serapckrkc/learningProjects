6aa80f99c9828c1d7d32526cda7a6fc73148071e
Serapckrkc <Serapckrkc@gmail.com>
2022-01-28 00:09:19 +0300

    took value from input and pushed array

---
+++  LocalStorage/app.js
@@ -0,0 +1,28 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name = item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  populateList(items, itemsList),
+  this.reset();
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+    <li>
+       <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+       <label for="item${i}"> ${plate.text}</label>
+    </li>
+    `;
+  }).join('');
+}
+
+ addItems.addEventListener('submit', addItem)
