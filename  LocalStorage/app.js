ff3d6a4ba2c2b609059f5b0a8a03bf77cebc2214
Serapckrkc <Serapckrkc@gmail.com>
2022-01-28 23:00:10 +0300

    project14 finished

---  LocalStorage/app.js
+++  LocalStorage/app.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -11,9 +11,10 @@ function addItem(e) {
   };
   items.push(item);
   populateList(items, itemsList),
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset();
 }
-
+ 
 function populateList(plates = [], platesList) {
   platesList.innerHTML = plates.map((plate, i) => {
     return `
@@ -25,4 +26,17 @@ function populateList(plates = [], platesList) {
   }).join('');
 }
 
- addItems.addEventListener('submit', addItem)
+function toggleDone(e) {
+  if (!e.target.matches('input')) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+
+}
+
+ addItems.addEventListener('submit', addItem);
+ itemsList.addEventListener('click', toggleDone);
+
+ populateList(items, itemsList);
